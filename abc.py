a86c257bc5ca22ad9dc6cdbfd1edd57e13d78800
Sonallgupta <126663119+Sonallgupta@users.noreply.github.com>
2023-05-17 21:30:12 +0530

    Update abc.py
    Python program which accepts the radius of a circle from the user and computes area.

--- abc.py
+++ abc.py
@@ -1,3 +1,4 @@
+#write a Python program which accepts the radius of a circle from the user and computes area.
 from math import pi
 r = float(input ("Input the radius of the circle : "))
 Area = str(pi * r**2);
